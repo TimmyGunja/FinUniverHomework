a1d10c328a1a00ae41ff880e673707072cd59d8a
timmygunja <t.urusov13@mail.ru>
2020-09-24 20:12:34 +0300

    10_09_2020

--- src/com/company/Vector.java
+++ src/com/company/Vector.java
@@ -1,6 +1,9 @@
 package com.company;
 
 
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Vector {
     int x;
     int y;
@@ -22,6 +25,7 @@ public class Vector {
         System.out.println(vectorLength(v1));
         System.out.println(scalar(v1, v2));
 //        System.out.println(vectorMult(v1, v2));
+        randomArrays(5);
     }
 
     static double vectorLength(Vector v){
@@ -35,5 +39,27 @@ public class Vector {
     static double vectorMult(Vector v1, Vector v2){
         double vmult = v1.y*v2.z - v1.y*v2.y;
         return vmult;
+    }
+
+    static Vector vectorSum(Vector v1, Vector v2){
+        int[] coordArray = {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
+        return new Vector(coordArray);
+    }
+
+    static Vector vectorSub(Vector v1, Vector v2){
+        int[] coordArray = {v1.x - v2.x, v1.y - v2.y, v1.z - v2.z};
+        return new Vector(coordArray);
+    }
+
+    static Vector[] randomArrays(int N){
+        Vector[] randArrays = new Vector[N];
+        for (int i = 0; i < N; i++){
+            int x = ThreadLocalRandom.current().nextInt(-100, 100 + 1);
+            int y = ThreadLocalRandom.current().nextInt(-100, 100 + 1);
+            int z = ThreadLocalRandom.current().nextInt(-100, 100 + 1);
+            randArrays[i] = new Vector(new int[]{x, y, z});
+            System.out.println(Arrays.asList(randArrays[i]));
+        }
+        return randArrays;
     }
 }
